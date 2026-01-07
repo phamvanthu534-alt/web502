3d26e67622606551925bf62f2561fbfd1d1f99f8
lap1 <phamvanthu534@gmail.com>
2026-01-07 14:21:14 +0700

    Update lab1 TypeScript exercises

--- learn-typescript/src/lab1.ts
+++ learn-typescript/src/lab1.ts
@@ -1,5 +1,20 @@
-// Viết 1 hàm trả về chu vi và diện tích hình chữ nhật
-// Viết 1 hàm tính tổng nhiều số (không biết trước số lượng tham số), sử dụng rest parameter
-// Viết hàm trả về số lượng xuất hiện của 1 kí tự trong chuỗi
-// Viết hàm trả về boolean kiểm tra 1 số có phải số nguyên tố
-// funtion ChuViHCN(){}
+// 1. Viết 1 hàm trả về chu vi và diện tích hình chữ nhật
+function chuViDienTichHCN(chieuDai: number, chieuRong: number) {
+    const chuVi = (chieuDai + chieuRong) * 2;
+    const dienTich = chieuDai * chieuRong;
+    return { chuVi, dienTich };
+  }
+  
+  console.log(chuViDienTichHCN(5, 10));
+  
+  
+  // 2. Viết 1 hàm tính tổng nhiều số (không biết trước số lượng tham số)
+
+
+  
+  
+  // 3. Viết hàm trả về số lượng xuất hiện của 1 kí tự trong chuỗi
+  
+  
+  
+  // 4. Viết hàm trả về boolean kiểm tra 1 số có phải số nguyên tố
